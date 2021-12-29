ae7e397a50546745a394989731b06007bd4a8ec8
CyrilKamal <43249970+CyrilKamal@users.noreply.github.com>
2021-12-28 23:30:26 -0500

    fixed some more warnings for deployment

--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -5,14 +5,14 @@ import "./CollectionCard.css";
 const CollectionCard = ({ id, name, traits, image }) => {
   return (
     <div className="collectionCard">
-      <img src={image} />
+      <img src={image} alt="" />
       <div className="details">
         <div className="name">
           {name} <div className="id">#{id}</div>
         </div>
 
         <div className="priceContainer">
-          <img src={weth} className="wethImage" />
+          <img src={weth} className="wethImage" alt="" />
           <div className="price">{traits[0]?.value}</div>
         </div>
       </div>
