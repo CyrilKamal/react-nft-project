c5fba5e57adc1161138229e13c89592f6e11a110
CyrilKamal <43249970+CyrilKamal@users.noreply.github.com>
2021-12-28 23:38:14 -0500

    fixed more problems with deployment

--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -5,14 +5,14 @@ import "./CollectionCard.css";
 const CollectionCard = ({ id, name, traits, image }) => {
   return (
     <div className="collectionCard">
-      <img src={image} alt="" />
+      <img src={image} alt="collection image" />
       <div className="details">
         <div className="name">
           {name} <div className="id">#{id}</div>
         </div>
 
         <div className="priceContainer">
-          <img src={weth} className="wethImage" alt="" />
+          <img src={weth} className="wethImage" alt="weth logo" />
           <div className="price">{traits[0]?.value}</div>
         </div>
       </div>
